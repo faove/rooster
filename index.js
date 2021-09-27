d738c9b47d3c3443599f1f33b0dc6d971165f90d
Francisco Alvarez <faove@hotmail.com>
2021-09-26 21:44:35 -0300

    test

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const ccxt = require('ccxt');
 const axios = require('axios');
 
 const tick = async(config,binanceClient) => {
+    
     const {asset, base, allocation, spread, tickInterval} = config;
     //const {apiKey, apiSecret} = binanceClient;
     const market = `${asset}/${base}`;
@@ -35,16 +36,33 @@ const tick = async(config,binanceClient) => {
     //     await binanceClient.cancelOrder(order.id);
     // });
 
-    const results = await Promise.all([
-        axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd'),
-        axios.get('https://api.coingecko.com/api/v3/simple/price?ids=tether&vs_currencies=usd'),
-    ]);
+    // const results = await Promise.all([
+    //     axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd'),
+    //     axios.get('https://api.coingecko.com/api/v3/simple/price?ids=tether&vs_currencies=usd'),
+    // ]);
     // console.log(results[0].data.bitcoin.usd);
     // console.log(results[0].data.tether.usd);
-    const marketPrice = results[0].data.bitcoin.usd / results[1].data.tether.usd;
+    // const marketPrice = results[0].data.bitcoin.usd / results[1].data.tether.usd;
 
-    const sellPrice = marketPrice * (1 + spread);
-    const buyPrice  = marketPrice * (1 - spread);
+    //console.log (binanceClient.id,  await binanceClient.fetchTicker(market))
+    
+    // console.log(await binanceClient.loadMarkets())
+    //console.log(await binanceClient.fetchOrderBook(market))
+    let orderbook = await binanceClient.fetchOrderBook(market);
+    // let bid = orderbook.bids.length ? orderbook.bids[0][0] : undefined
+    // let ask = orderbook.asks ? orderbook.asks[0][0] : undefined
+    // let spread1 = (bid && ask) ? ask - bid : undefined
+    // console.log (binanceClient.id, 'market price', { bid, ask, spread1 })
+    // console.log (binanceClient.id, 'market price', { orderbook })
+    console.log (binanceClient.id, orderbook.bids[0])
+    
+    //fetchtrades trae array de buy y sell 
+    // console.log (binanceClient.id,  await binanceClient.fetchTrades(market))
+    
+    // 
+    // console.log (binanceClient.id,  await binanceClient.fetchCurrencies())
+    // const sellPrice = marketPrice * (1 + spread);
+    // const buyPrice  = marketPrice * (1 - spread);
     const balances  = await binanceClient.fetchBalance();
 
     // console.log(`
@@ -58,35 +76,45 @@ const tick = async(config,binanceClient) => {
 
     
     const assetBalance = balances.free[asset];
-    console.log(assetBalance);
+    // console.log(assetBalance);
     const baseBalance = balances.free[base];
     
-    console.log(baseBalance);
+    // console.log(baseBalance);
     
-    const sellVolumen = assetBalance * allocation;
-    const buyVolumen  = (baseBalance* allocation) / marketPrice;
+    // const sellVolumen = assetBalance * allocation;
+    // const buyVolumen  = (baseBalance* allocation) / marketPrice;
 
     // await binanceClient.createLimitSellOrder(market,sellVolumen, sellPrice);
     // await binanceClient.createLimitBuyOrder(market, buyVolumen, buyPrice);
 
-    console.log(`
-        Nuevo ticket para ${market}...
-        assetBalance: ${assetBalance}
-        baseBalance: ${baseBalance}
-        marketPrice: ${marketPrice}
-        sellPrice: ${sellPrice}
-        buyPrice: ${buyPrice}
-        Creada orden de venta límite por ${sellVolumen}@${sellPrice}
-        Creada orden de compra límite por ${buyVolumen}@${buyPrice}
-    `);
+    // console.log(`
+    //     Nuevo ticket para ${market}...
+    //     assetBalance: ${assetBalance}
+    //     baseBalance: ${baseBalance}
+    //     marketPrice: ${marketPrice}
+    //     sellPrice: ${sellPrice}
+    //     buyPrice: ${buyPrice}
+    //     Creada orden de venta límite por ${sellVolumen}@${sellPrice}
+    //     Creada orden de compra límite por ${buyVolumen}@${buyPrice}
+    // `);
     //market_order_placement = bittrex.create_market_sell_order ('ETH / BTC', .001)
+
+     // sell 1 BTC/USD for market price, sell a bitcoin for dollars immediately
+     // console.log (okcoinusd.id, await okcoinusd.createMarketSellOrder ('BTC/USD', 1))
+
+     // buy 1 BTC/USD for $2500, you pay $2500 and receive ฿1 when the order is closed
+     // console.log (okcoinusd.id, await okcoinusd.createLimitBuyOrder ('BTC/USD', 1, 2500.00))
+ 
+     // pass/redefine custom exchange-specific order params: type, amount, price or whatever
+     // use a custom order type
+     // bitfinex.createLimitSellOrder ('BTC/USD', 1, 10, { 'type': 'trailing-stop' })
 
 }
 
 const run = () => {
     
     const config = {
-        asset: 'SHIB',
+        asset: 'BTC',
         base: 'USDT',
         allocation: 0.1,
         spread: 0.2,
@@ -101,7 +129,10 @@ const run = () => {
         'secret': process.env.API_SECRET,
         'enableRateLimit': true,
     });
-    
+    // Quitar 
+    // binanceClient.setSandboxMode(true);
+    // console.log (binanceClient.requiredCredentials) // prints required credentials
+    // binanceClient.checkRequiredCredentials()    
     //console.log(binanceClient);
     // tick(config,binanceClient);
 
